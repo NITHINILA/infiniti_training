60b7aca73f17c43b68257768871f73c9e30345b5
NITHINILA <50821006+NITHINILA@users.noreply.github.com>
2019-05-20 19:59:48 +0530

    Create vowel_consonant.c

---
+++ vowel_consonant.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+char ca;
+scanf("%c",&ca);
+if((ca>'a'&&ca<'z') || (ca>'A'&&ca<'Z'))
+{
+
+    if(ca=='a' || ca=='e' || ca=='i' || ca=='o' || ca=='u' || ca=='A' || ca=='E' || ca=='I' || ca=='O' || ca=='U')
+    {
+        printf("Vowel");
+    }
+        else 
+        {
+            printf("Consonant");
+        }
+}
+else
+{
+	printf("invalid");
+}    
+}
+
