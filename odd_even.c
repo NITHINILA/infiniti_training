a1609d01e146282a4e9faaa2ddbe50dba388235c
NITHINILA <50821006+NITHINILA@users.noreply.github.com>
2019-05-20 19:04:51 +0530

    Create odd_even.c

---
+++ odd_even.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	int num;
+	scanf("%d",&number1);
+	if(num<0)
+	{
+		printf("invalid");
+	}
+	else if(num%2==0)
+	{
+		printf("Even");
+	}
+	else
+	{
+		printf("Odd");
+	}
+}
+
+
+
+
+
+
