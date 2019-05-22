bf0f417798e42d21c51aecac0a56e651d0f2a1e4
NITHINILA <50821006+NITHINILA@users.noreply.github.com>
2019-05-22 12:45:05 +0530

    Create armstron_bwtwo.c

---
+++ armstron_bwtwo.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,temp,i,base,sum;
+scanf("%d %d",&n1,&n2);
+for(i=n1;i<n2;i++)
+{
+	temp=i;
+	sum=0;
+	while(temp!=0)
+	{
+		base=temp%10;
+		sum=sum+(base*base*base);
+		temp=temp/10;
+	}
+	if(i==sum)
+	{
+		printf("%d ", i);
+	}
+}
+}
