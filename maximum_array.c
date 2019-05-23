49f0d6e5e87c3107ce721c7a74767404733f669d
NITHINILA <50821006+NITHINILA@users.noreply.github.com>
2019-05-23 09:55:51 +0530

    Create maximum_array.c

---
+++ maximum_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+int arr[50],size,i,maximum;
+scanf("%d",&size);
+for(i=0;i<size;i++)
+{
+	scanf("%d",&arr[i]);
+}
+	
+for(i=1;i<size;i++)
+	{
+		if(arr[0]<arr[i])
+		    arr[0]=arr[i];
+	}
+	printf("%d",arr[0]);
+}
