821e8dcff38c74a7df9dae190af60b57e1d2bcc5
NITHINILA <50821006+NITHINILA@users.noreply.github.com>
2019-05-23 11:20:38 +0530

    Create isnumeric.c

---
+++ isnumeric.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+    void main()
+    {
+    char c;
+    scanf("%c",&c);
+    if(isdigit(c))
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    }
